22f0e6d35449e3ef9db8293906cd997600e3ecd9
Kacper Grzywacz <kacpergrzywacz2@gmail.com>
2025-12-12 23:16:40 +0100

    update: clean App.jsx and add Navigation component

--- App.jsx
+++ App.jsx
@@ -1,24 +1,5 @@
-import { Text, View, StyleSheet } from 'react-native';
-import TipOfTheDay from './TipOfTheDay';
-
+import Navigation from './navigation/Navigation';
 
 export default function Index() {
-  return (
-    <View style={styles.container}>
-      <Text style={styles.text}>Splash screen for the application</Text>
-      <TipOfTheDay />
-    </View>
-  );
+  return <Navigation />;
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#25292e',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  text: {
-    color: '#fff',
-  },
-});
