07af04951491836d312ab6e663e55708769583cc
Kacper Grzywacz <kacpergrzywacz2@gmail.com>
2025-12-12 23:11:58 +0100

    chore: configure React Navigation stack

---
+++ navigation/Navigation.jsx
@@ -0,0 +1,12 @@
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
+const Stack = createNativeStackNavigator();
+
+export default function Navigation() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName='FirstScreen'></Stack.Navigator>
+    </NavigationContainer>
+  );
+}
