18581f891d99875be0a1d4c734ffd2e2024a732c
Kacper Grzywacz <kacpergrzywacz2@gmail.com>
2025-12-13 00:36:36 +0100

    add: AboutScreen with personal data and active mail

--- navigation/Navigation.jsx
+++ navigation/Navigation.jsx
@@ -2,6 +2,7 @@ import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import FirstScreen from '../screens/FirstScreen';
 import NoteEditorScreen from '../screens/NoteEditorScreen';
+import AboutScreen from '../screens/AboutScreen';
 
 const Stack = createNativeStackNavigator();
 
@@ -9,7 +10,7 @@ export default function Navigation() {
   return (
     <NavigationContainer>
       <Stack.Navigator initialRouteName='Home'>
-        <Stack.Screen name='Home' component={FirstScreen} />
+        <Stack.Screen name='Home' component={AboutScreen} />
         <Stack.Screen name='EditNote' component={NoteEditorScreen} />
       </Stack.Navigator>
     </NavigationContainer>
---
+++ screens/AboutScreen.jsx
@@ -0,0 +1,50 @@
+import { StyleSheet, View, Text, Linking } from 'react-native';
+
+export default function AboutScreen() {
+  const email = 'kacper2@gmail.com';
+
+  const sendEmail = () => {
+    Linking.openURL(`mailto:${email}`);
+  };
+
+  return (
+    <View style={styles.container}>
+      <Text style={styles.name}>Kacper Grzywacz</Text>
+      <Text style={styles.phoneNumber}>738291938</Text>
+      <Text style={styles.email} onPress={sendEmail}>
+        {email}
+      </Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#f5f5f5',
+    padding: 20,
+  },
+  name: {
+    fontSize: 22,
+    fontWeight: 'bold',
+    marginBottom: 15,
+  },
+  phoneNumber: {
+    fontSize: 18,
+    color: '#333',
+    marginBottom: 15,
+  },
+  email: {
+    fontSize: 18,
+    color: '#fff',
+    backgroundColor: '#228aebff',
+    paddingVertical: 8,
+    paddingHorizontal: 16,
+    borderRadius: 8,
+    fontWeight: '700',
+    overflow: 'hidden',
+    textAlign: 'center',
+  },
+});
