e6cdd02de0036958502bcedeec24544ed00396a0
Kacper Grzywacz <kacpergrzywacz2@gmail.com>
2025-12-13 00:24:28 +0100

    update: Navigation add Stack screen edit note

--- navigation/Navigation.jsx
+++ navigation/Navigation.jsx
@@ -1,6 +1,7 @@
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import FirstScreen from '../screens/FirstScreen';
+import NoteEditorScreen from '../screens/NoteEditorScreen';
 
 const Stack = createNativeStackNavigator();
 
@@ -9,6 +10,7 @@ export default function Navigation() {
     <NavigationContainer>
       <Stack.Navigator initialRouteName='Home'>
         <Stack.Screen name='Home' component={FirstScreen} />
+        <Stack.Screen name='EditNote' component={NoteEditorScreen} />
       </Stack.Navigator>
     </NavigationContainer>
   );
