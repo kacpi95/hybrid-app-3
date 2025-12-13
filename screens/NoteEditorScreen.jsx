2d151228ab04a1bc93de8c1ee4531415d26f71d3
Kacper Grzywacz <kacpergrzywacz2@gmail.com>
2025-12-13 01:07:01 +0100

    update: add localStorage, useEffect and function to saveNote in NoteEditor

--- screens/NoteEditorScreen.jsx
+++ screens/NoteEditorScreen.jsx
@@ -1,8 +1,41 @@
-import { useState } from 'react';
-import { View, StyleSheet, TextInput } from 'react-native';
+import { useEffect, useState } from 'react';
+import {
+  View,
+  StyleSheet,
+  TextInput,
+  TouchableOpacity,
+  Alert,
+  Text,
+} from 'react-native';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
 export default function NoteEditorScreen() {
   const [note, setNote] = useState('');
+
+  const STORAGE_KEY = `@user_note`;
+
+  useEffect(() => {
+    const loadNote = async () => {
+      try {
+        const savedNote = await AsyncStorage.getItem(STORAGE_KEY);
+        if (savedNote !== null) {
+          setNote(savedNote);
+        }
+      } catch (err) {
+        Alert.alert('Failed to load the note');
+      }
+    };
+    loadNote();
+  }, []);
+
+  const saveNote = async () => {
+    try {
+      await AsyncStorage.setItem(STORAGE_KEY, note);
+      Alert.alert('Note save');
+    } catch (err) {
+      Alert.alert('Failed to save note');
+    }
+  };
 
   return (
     <View style={styles.container}>
@@ -13,7 +46,10 @@ export default function NoteEditorScreen() {
         textAlignVertical='top'
         value={note}
         onChangeText={setNote}
-      ></TextInput>
+      />
+      <TouchableOpacity style={styles.button} onPress={saveNote}>
+        <Text style={styles.buttonText}>Save</Text>
+      </TouchableOpacity>
     </View>
   );
 }
@@ -30,5 +66,17 @@ const styles = StyleSheet.create({
     padding: 10,
     fontSize: 16,
     borderRadius: 8,
+    marginBottom: 15,
+  },
+  button: {
+    backgroundColor: 'rgba(167, 167, 167, 0.86)',
+    paddingVertical: 12,
+    borderRadius: 8,
+    alignItems: 'center',
+  },
+  buttonText: {
+    color: '#fff',
+    fontSize: 16,
+    fontWeight: 'bold',
   },
 });
